51fe99955d0f9501e2c441f464e8359560bb8f2d
EMMA LU <luzhuyi13169@gmail.com>
2022-06-27 23:46:17 +1000

    hi

---
+++ src/components/DivItem.js
@@ -0,0 +1,13 @@
+function DivItem() {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+      </div>
+      <div>Amount</div>
+    </div>
+  );
+}
+
+export default DivItem;
